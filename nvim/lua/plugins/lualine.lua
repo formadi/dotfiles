119f9489f11db66ad33d44ec87a414983b6584f6
formadi <bluesky4639@gmail.com>
2023-12-10 00:53:33 +0900

    lualine hide encoding info

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -62,7 +62,7 @@ return {
             cond = lazy_status.has_updates,
             color = { fg = "#ff9e64" },
           },
-          { "encoding" },
+          -- { "encoding" },
           { "fileformat", symbols = { unix =" " } },
           { "filetype" },
         },
