fc06899086f29980b2f32fc7d3f9588f2ea30129
formadi <bluesky4639@gmail.com>
2023-12-09 18:04:39 +0900

    code cleanup

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,3 +2,21 @@ require("core")
 
 -- vim.g.python3_host_prog = "/usr/bin/python3"
 vim.g.python3_host_rog = "/opt/homebrew/bin/python3"
+
+
+-- if vim.g.neovide then
+--   vim.o.guifont = "Sarasa Term K Nerd Font ExtraLight:h12"
+--   vim.g.neovide_padding_top   = 10
+--   vim.g.neovide_padding_down  = 10
+--   vim.g.neovide_padding_left  = 10
+--   vim.g.neovide_padding_right = 10
+--
+--   local alpha = function ()
+--     return string.format("%x", math.floor(0.8))
+--   end
+--
+--   vim.g.neovide_transparency = 0.0
+--   vim.g.tranparency = 1.0
+--   vim.g.neovide_background_color = "#242e41" .. alpha()
+-- end
+
