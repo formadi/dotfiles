b0032d3f29ce4568a81ebde55ab3d0bfbe083719
formadi <bluesky4639@gmail.com>
2023-12-09 21:34:07 +0900

    add todo-comment keymap "TodoQuickFix"

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -3,5 +3,11 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   config = function()
     require("todo-comments").setup()
+
+    -- keywords
+    -- TODO , HACK, WARN, PREF, NOTE, TEST
+
+    -- set keymaps
+    vim.keymap.set("n", "<leader>tq", "<cmd>TodoQuickFix<CR>", { desc = "Todo Quick Fix" })
   end,
 }
