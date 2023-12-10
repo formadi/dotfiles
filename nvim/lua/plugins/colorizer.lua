496f3bccc4b59833d6e92fdf03754939e356e54b
formadi <bluesky4639@gmail.com>
2023-12-10 20:56:56 +0900

    change mode : background -> virtualtext

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -1,5 +1,14 @@
 return {
   "NvChad/nvim-colorizer.lua",
   event  = { "BufReadPre", "BufNewFile" },
-  config = true,
+  -- config = true,
+  config = function()
+    require('colorizer').setup ({
+      user_default_options = {
+        mode = "virtualtext",
+        virtualtext = "􀏄",
+      },
+    })
+  end,
+
 }
