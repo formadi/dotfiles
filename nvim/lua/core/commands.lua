971c923184ed9c315d394d5b5e42e9304f52bf63
formadi <bluesky4639@gmail.com>
2023-12-11 16:48:16 +0900

    use embedded Fold, disable nvim-ufo plugin

--- nvim/lua/core/commands.lua
+++ nvim/lua/core/commands.lua
@@ -28,3 +28,19 @@ vim.cmd [[
   :autocmd InsertEnter * set nocul
   :autocmd InsertLeave * set cul
 ]]
+
+
+
+-- fold setting
+vim.o.foldlevel      = 99
+vim.o.foldcolumn     = '2'
+vim.wo.foldnestmax   = 10
+vim.wo.foldminlines  = 1
+vim.o.foldenable     = true
+vim.o.foldlevelstart = 99
+vim.o.foldtext       = [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').' ... ' . '󱝁  ' . (v:foldend - v:foldstart + 1) . ' lines']]
+vim.wo.foldmethod    = "indent"
+vim.wo.foldexpr      = "nvim_treesitter#foldexpr()"
+vim.o.fillchars      = [[eob: ,fold: ,foldopen:▽,foldsep:┊,foldclose:▷]]
+vim.cmd('highlight Folded     ctermbg=NONE guibg=NONE')
+vim.cmd('highlight FoldColumn ctermfg=NONE guifg=NONE')
