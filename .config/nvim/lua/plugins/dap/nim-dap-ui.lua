85dd677eff39ac2bfc3104de7988e396b78e442c
formadi <bluesky4639@gmail.com>
2023-12-18 01:16:19 +0900

    add new dap keymaps

--- .config/nvim/lua/plugins/dap/nim-dap-ui.lua
+++ .config/nvim/lua/plugins/dap/nim-dap-ui.lua
@@ -14,14 +14,19 @@ return {
       dap.listeners.before.event_terminated["depui_config"] = function()
         dapui.close()
       end
-
       dap.listeners.before.event_exited["dapui_config"] = function()
         dapui.close()
       end
 
-      vim.keymap.set("n", "<leader>dt", ':DapToggleBreakpoint<CR>')
-      vim.keymap.set("n", "<leader>dx", ':DapTerminate<CR>')
-      vim.keymap.set("n", "<leader>do", ':DapStepOver<CR>')
+      -- set keymaps
+      -- vim.keymap.set("n", "<leader>dt", ':DapToggleBreakpoint<CR>',  { desc = "DAP - toggle break point"})
+      vim.keymap.set("n", "<leader>dx", ':DapTerminate<CR>'       ,  { desc = "DAP - terminate"})
+      -- vim.keymap.set("n", "<leader>do", ':DapStepOver<CR>'        ,  { desc = "DAP - step over"})
+      vim.keymap.set("n", "<leader>dc", "<Cmd>lua require'dap'.continue()<CR>"          ,  { desc = "DAP - continue"})
+      vim.keymap.set("n", "<leader>do", "<Cmd>lua require'dap'.step_over()<CR>"         ,  { desc = "DAP - step over"})
+      vim.keymap.set("n", "<leader>di", "<Cmd>lua require'dap'.step_into()<CR>"         ,  { desc = "DAP - step into"})
+      vim.keymap.set("n", "<leader>dt", "<Cmd>lua require'dap'.step_out()<CR>"          ,  { desc = "DAP - step out"})
+      vim.keymap.set("n", "<leader>db", "<Cmd>lua require'dap'.toggle_breakpoint()<CR>" ,  { desc = "DAP - toggle breakpoint"})
 
   end,
 }
