02d1fd85689422021d613919bc15d7912decb0c2
formadi <bluesky4639@gmail.com>
2023-12-18 16:46:28 +0900

    .

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -23,19 +23,20 @@ return {
       ["<leader>"] = {
         n = {
           name = "Noice / Neorg",
+          n = { "<cmd>Neorg<CR>", "Neorg" },
           j = {
             name = "Neorg Journal",
             t = { "<cmd>Neorg journal today<CR>"    , "journal 'today'"     },
             y = { "<cmd>Neorg journal yesterday<CR>", "journal 'yesterday'" },
             n = { "<cmd>Neorg journal tomorrow<CR>" , "journal 'tomorrow'"  },
           },
-          w = {
-            name = "Neorg Workspace",
-            w = { "<cmd>Neorg workspace work_notes<CR>"     , "workspace 'work notes'"      },
-            m = { "<cmd>Neorg workspace main<CR>"           , "workspace 'main'"            },
-            k = { "<cmd>Neorg workspace knowledge_base<CR>" , "workspace 'knowledge base'"  },
-            c = { "<cmd>Neorg workspace config<CR>"         , "workspace 'config'"          },
-          },
+          -- w = {
+          --   name = "Neorg Workspace",
+          --   n = { "<cmd>Neorg workspace work_notes<CR>"     , "workspace 'work notes'"      },
+          --   m = { "<cmd>Neorg workspace main<CR>"           , "workspace 'main'"            },
+          --   k = { "<cmd>Neorg workspace knowledge_base<CR>" , "workspace 'knowledge base'"  },
+          --   c = { "<cmd>Neorg workspace config<CR>"         , "workspace 'config'"          },
+          -- },
         },
       },
     })
