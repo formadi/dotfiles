36eb9b90fde6294923d98436f5490ed78a08e744
formadi <bluesky4639@gmail.com>
2023-12-20 08:52:10 +0900

    .

--- .config/nvim/lua/plugins/alpha-nvim.lua
+++ .config/nvim/lua/plugins/alpha-nvim.lua
@@ -42,11 +42,11 @@ return {
     -- Set menu
     dashboard.section.buttons.val = {
       dashboard.button("e",      "  > New File",                              "<cmd>ene<CR>"),
-      dashboard.button("SPC ee", "  > Toggle file explorer",                  "<cmd>NvimTreeToggle<CR>"),
-      dashboard.button("SPC ff", "󰱼 > Find File",                              "<cmd>Telescope find_files<CR>"),
-      dashboard.button("SPC fs", "  > Find Word",                             "<cmd>Telescope live_grep<CR>"),
+      dashboard.button("SPC jt", "  > Journal - Today",                       "<cmd>Neorg journal today<CR>"),
+      dashboard.button("SPC jm", "  > Journal - Tommorrow",                   "<cmd>Neorg journal tomorrow<CR>"),
+      dashboard.button("SPC jy", "  > Journal - Yesterday",                   "<cmd>Neorg journal yesterday<CR>"),
       dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
-      dashboard.button("q",      " > Quit NVIM",                              "<cmd>qa<CR>"),
+      dashboard.button("q",      "  > Quit NVIM",                             "<cmd>qa<CR>"),
     }
 
     -- Send config to alpha
