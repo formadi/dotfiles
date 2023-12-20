9f0f6d5dec40877d51393bcb2107ff95d6b9faf7
formadi <bluesky4639@gmail.com>
2023-12-20 20:00:02 +0900

    keymap 추가

--- .config/nvim/lua/plugins/neorg.lua
+++ .config/nvim/lua/plugins/neorg.lua
@@ -10,6 +10,16 @@ return {
  				["core.ui"] = {},
 				-- ["core.ui.calendar"] = {},
 				-- ["core.tempus"] = {},
+        ['core.journal'] = {
+          config = {
+            strategy = "flat",
+          },
+        },
+        ['core.esupports.metagen'] = {
+          config = {
+            type = "auto", -- generates metadata if it is not present
+          },
+        },
         ["core.dirman"] = { -- Manages Neorg workspaces
           config = {
             workspaces = {
@@ -31,26 +41,32 @@ return {
       ["<leader>"] = {
         n = {
           name = "Noice / Neorg",
-          n = { "<cmd>Neorg<CR>"                 , "Neorg" },
-          c = { "<cmd>Neorg toggle-concealer<CR>", "Neorg toggle concealer" },
-          i = { "<cmd>Neorg index<CR>"           , "Neorg index" },
+          n = { "<cmd>Neorg<CR>"                 ,                                    "Neorg popup"              },
+          c = { "<cmd>Neorg toggle-concealer<CR>",                                    "toggle concealer"         },
+          i = { "<cmd>Neorg index<CR>"           ,                                    "goto index"               },
+          s = { "<cmd>Neorg generate-workspace-summary journal notes rust<CR>",       "generate index"           },  -- exclusive main
           k = {
             name = "Neorg keybind",
-            l = { "<cmd>Neorg keybind all core.looking-glass.magnify-code-block<CR>", "Neorg looking-glass"},
-            n = { "<cmd>Neorg keybind all core.dirman.new.note<CR>"                 , "Neorg new note"     },
+            l = { "<cmd>Neorg keybind all core.looking-glass.magnify-code-block<CR>", "looking-glass"            },
+            n = { "<cmd>Neorg keybind all core.dirman.new.note<CR>"                 , "new note"                 },
           },
           j = {
             name = "Neorg Journal",
-            t = { "<cmd>Neorg journal today<CR>"    , "journal 'today'"     },
-            y = { "<cmd>Neorg journal yesterday<CR>", "journal 'yesterday'" },
-            n = { "<cmd>Neorg journal tomorrow<CR>" , "journal 'tomorrow'"  },
+            t = { "<cmd>Neorg journal today<CR>"    ,                                 "journal 'today'"          },
+            y = { "<cmd>Neorg journal yesterday<CR>",                                 "journal 'yesterday'"      },
+            n = { "<cmd>Neorg journal tomorrow<CR>" ,                                 "journal 'tomorrow'"       },
           },
           w = {
             name = "Neorg Workspace",
-            r = { "<cmd>Neorg workspace rust<CR>"     , "workspace 'rust'"         },
-            n = { "<cmd>Neorg workspace notes<CR>"    , "workspace 'notes(main)'"  },
-          --   k = { "<cmd>Neorg workspace knowledge_base<CR>" , "workspace 'knowledge base'"  },
-          --   c = { "<cmd>Neorg workspace config<CR>"         , "workspace 'config'"          },
+            r = { "<cmd>Neorg workspace rust<CR>"     ,                               "workspace 'rust'"         },
+            n = { "<cmd>Neorg workspace notes<CR>"    ,                               "workspace 'notes'"        },
+            j = { "<cmd>Neorg workspace journal<CR>"  ,                               "workspace 'journal'"      },
+            m = { "<cmd>Neorg workspace main<CR>"  ,                                  "workspace 'main'"         },
+          },
+          m = {
+            name = "Neorg Metagen",
+            i = { "<cmd>Neorg inject-metadata<CR>" ,                                  "inject metadata"         },
+            u = { "<cmd>Neorg update-metadata<CR>" ,                                  "update metadata"         },
           },
         },
       },
