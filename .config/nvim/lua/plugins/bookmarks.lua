6e77397838bfde564903e42a9f27cb6ebabe6f8c
formadi <bluesky4639@gmail.com>
2023-12-25 17:34:42 +0900

    add new bookmarks plugin

---
+++ .config/nvim/lua/plugins/bookmarks.lua
@@ -0,0 +1,31 @@
+
+return {
+  "tomasky/bookmarks.nvim",
+  event = "VimEnter",
+  config = function()
+    require("bookmarks").setup ({
+      on_attach = function(bufnr)
+        local bm = require "bookmarks"
+        local map = vim.keymap.set
+        map("n","<leader>bt",bm.bookmark_toggle, { desc = "toggle" } ) -- add or remove bookmark at current line
+        map("n","<leader>ba",bm.bookmark_ann,    { desc = "add"    } )  -- add or edit mark annotation at current line
+        map("n","<leader>bc",bm.bookmark_clean,  { desc = "clean"  } )  -- clean all marks in local buffer
+        map("n","<leader>bn",bm.bookmark_next,   { desc = "next"   } )  -- jump to next mark in local buffer
+        map("n","<leader>bp",bm.bookmark_prev,   { desc = "prev"   } )  -- jump to previous mark in local buffer
+        map("n","<leader>bl",bm.bookmark_list,   { desc = "list"   } )  -- show marked file list in quickfix window
+      end
+    })
+
+    -- set keymaps
+    local wk = require("which-key")
+    wk.register({
+      ["<leader>"] = {
+        b = {
+          name = "Bookmarks",
+        },
+      },
+    })
+
+
+  end,
+}
