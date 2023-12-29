0999a3dfdb5fec2feb0f843089d4fb8a09bafd2e
formadi <bluesky4639@gmail.com>
2023-12-30 01:52:57 +0900

    excude filetype - norg

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -33,7 +33,16 @@ return {
     end)
 
     vim.g.rainbow_delimiters = { highlight = highlight }
-    require("ibl").setup({ scope = { highlight = highlight } })
+    require("ibl").setup({
+      scope = { highlight = highlight },
+      exclude = {
+        filetypes = {
+          "norg",
+        },
+      },
+    })
     hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+
+
   end,
 }
